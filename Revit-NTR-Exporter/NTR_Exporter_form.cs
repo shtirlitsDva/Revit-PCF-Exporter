d744022a74d064d05279f7e066af62126be05f43
Michail Golubjev <xopkop@gmail.com>
2017-04-08 14:39:08 +0200

    Revert "Making the form work before deleting everything"
    This reverts commit 8c742a7f3c44a17ab7da50bd3269ed37361d6e90.

--- Revit-NTR-Exporter/NTR_Exporter_form.cs
+++ Revit-NTR-Exporter/NTR_Exporter_form.cs
@@ -8,11 +8,10 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
 using BuildingCoder;
-using NTR_Exporter;
+using PCF_Functions;
 using mySettings = NTR_Exporter.Properties.Settings;
 using iv = NTR_Functions.InputVars;
 using dh = PCF_Functions.DataHandler;
-using PCF_Functions;
 
 namespace NTR_Exporter
 {
@@ -48,7 +47,7 @@ namespace NTR_Exporter
             //Init Scope
 
             //Gather all physical piping systems and collect distinct abbreviations
-            pipeLinesAbbreviations = PCF_Functions.MepUtils.GetDistinctPhysicalPipingSystemTypeNames(_doc);
+            pipeLinesAbbreviations = MepUtils.GetDistinctPhysicalPipingSystemTypeNames(_doc);
 
             //Use the distinct abbreviations as data source for the comboBox
             comboBox2.DataSource = pipeLinesAbbreviations;
@@ -138,13 +137,13 @@ namespace NTR_Exporter
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //iv.ExcelSheet = (string)comboBox1.SelectedItem;
-            ////mySettings.Default.excelWorksheetSelectedName = iv.ExcelSheet;
-            //DATA_TABLE = DATA_SET.Tables[iv.ExcelSheet];
-            //ParameterData.parameterNames = null;
-            //ParameterData.parameterNames = (from dc in DATA_TABLE.Columns.Cast<DataColumn>() select dc.ColumnName).ToList();
-            //ParameterData.parameterNames.RemoveAt(0);
-            //Util.InfoMsg("Following parameters will be initialized:\n" + string.Join("\n", ParameterData.parameterNames.ToArray()));
+            iv.ExcelSheet = (string)comboBox1.SelectedItem;
+            //mySettings.Default.excelWorksheetSelectedName = iv.ExcelSheet;
+            DATA_TABLE = DATA_SET.Tables[iv.ExcelSheet];
+            ParameterData.parameterNames = null;
+            ParameterData.parameterNames = (from dc in DATA_TABLE.Columns.Cast<DataColumn>() select dc.ColumnName).ToList();
+            ParameterData.parameterNames.RemoveAt(0);
+            Util.InfoMsg("Following parameters will be initialized:\n" + string.Join("\n", ParameterData.parameterNames.ToArray()));
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
