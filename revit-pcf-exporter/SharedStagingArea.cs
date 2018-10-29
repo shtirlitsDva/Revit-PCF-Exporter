b0206d4e39149335c7f48bd3b8ba8f87a78898f1
Michail Golubjev <xopkop@gmail.com>
2018-10-29 06:27:37 +0100

    cont 7

--- revit-pcf-exporter/SharedStagingArea.cs
+++ revit-pcf-exporter/SharedStagingArea.cs
@@ -4,6 +4,7 @@ using Autodesk.Revit.UI;
 using MoreLinq;
 using PCF_Functions;
 using PCF_Output;
+using Shared;
 using Shared.BuildingCoder;
 using System;
 using System.Collections;
@@ -16,5 +17,31 @@ namespace Shared
     public static class SharedStagingArea
     {
         public static string FamilyName(this Element e) => e.get_Parameter(BuiltInParameter.ELEM_FAMILY_PARAM).AsValueString();
+
+        /// <summary>
+        /// Return a hash string for a real number formatted to nine decimal places.
+        /// </summary>
+        public static string HashString(double a) => a.ToString("0.#########");
+
+        /// <summary>
+        /// Return a hash string for an XYZ point or vector with its coordinates
+        /// formatted to nine decimal places.
+        /// </summary>
+        public static string HashString(XYZ p)
+        {
+            return string.Format("({0},{1},{2})", HashString(p.X), HashString(p.Y), HashString(p.Z));
+        }
+
+        public class ConnectorXyzComparer : IEqualityComparer<Connector>
+        {
+            public bool Equals(Connector x, Connector y)
+            {
+                return null != x && null != y && Shared.Extensions.IsEqual(x.Origin, y.Origin);
+            }
+
+            public int GetHashCode(Connector x) => HashString(x.Origin).GetHashCode();
+        }
+
+
     }
 }
