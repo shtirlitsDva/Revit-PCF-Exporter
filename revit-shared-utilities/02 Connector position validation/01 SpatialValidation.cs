9019057aef7bfdda8477c44169566cf5573de9e5
Michail Golubjev <mgo@damgaard-ri.dk>
2019-03-08 23:36:15 +0100

    Problems with commiting again??

---
+++ revit-shared-utilities/02 Connector position validation/01 SpatialValidation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities
+{
+    class SpatialValidation
+    {
+        //Here be code to detect large deltas in Connector spatial groups... too late now anyway...
+    }
+}
