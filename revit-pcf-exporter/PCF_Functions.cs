2d115151480c27a2417297d817858c575c5cdd9c
Michail Golubjev <xopkop@gmail.com>
2023-03-11 13:00:07 +0100

    Revert "what?"
    This reverts commit 6976c3d2e3f1b5cf2b6255aa3b6d207091cef74c.

--- revit-pcf-exporter/PCF_Functions.cs
+++ revit-pcf-exporter/PCF_Functions.cs
@@ -1,19 +1,21 @@
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using MoreLinq;
+using Shared;
+using Shared.BuildingCoder;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-
-using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Plumbing;
-using Autodesk.Revit.UI;
-
-using MoreLinq;
-
-using Shared;
-using Shared.BuildingCoder;
-
+using System.Text.RegularExpressions;
 using iv = PCF_Functions.InputVars;
 using pdef = PCF_Functions.ParameterDefinition;
 using plst = PCF_Functions.ParameterList;
