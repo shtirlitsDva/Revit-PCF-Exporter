5fd50f705bba23ae38ffff833e6268032362a94b
Michail Golubjev <xopkop@gmail.com>
2023-03-12 09:25:49 +0100

    wqorking on modeless geometry validator

--- revit-shared-utilities/02 Connector position validation/01 SpatialValidation.cs
+++ revit-shared-utilities/02 Connector position validation/01 SpatialValidation.cs
@@ -127,34 +127,4 @@ namespace Shared.Tools
                 Math.Round(p.Z.FtToMm(), precision, MidpointRounding.AwayFromZero).ToString("#." + new string('0', precision), CultureInfo.GetCultureInfo("en-GB")));
         }
     }
-
-    [DataContract]
-    internal class connectorSpatialGroup
-    {
-        public List<Connector> Connectors = new List<Connector>();
-        [DataMember]
-        public int nrOfCons = 0;
-        [DataMember] //More of a debug property, maybe should be removed later on
-        public List<string> SpecList = new List<string>();
-        [DataMember] //More of a debug property
-        List<string> ListOfIds = null;
-        [DataMember]
-        public double longestDist = 0;
-        public List<(Connector c1, Connector c2, double dist)> pairs;
-        public (Connector c1, Connector c2, double dist) longestPair;
-
-        internal connectorSpatialGroup(IEnumerable<Connector> collection)
-        {
-            Connectors = collection.ToList();
-            nrOfCons = Connectors.Count();
-            ListOfIds = new List<string>(nrOfCons);
-            foreach (Connector con in collection)
-            {
-                Element owner = con.Owner;
-                Parameter par = owner.get_Parameter(new Guid("90be8246-25f7-487d-b352-554f810fcaa7")); //PCF_ELEM_SPEC parameter
-                if (par != null) { SpecList.Add(par.AsString()); }
-                ListOfIds.Add(owner.Id.ToString());
-            }
-        }
-    }
 }
