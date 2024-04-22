c43d56c6dcd9d0c16f9328769d5d7f24ea359eff
shtirlitsDva <xopkop@gmail.com>
2024-04-22 15:27:29 +0200

    small addition

---
+++ MEPUtils-SHARED/29 CreatePSLegend/01 CreatePSLegend.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+using MoreLinq;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Input;
+using dbg = Shared.Dbg;
+using fi = Shared.Filter;
+using lad = MEPUtils.CreateInstrumentation.ListsAndDicts;
+using mp = Shared.MepUtils;
+using tr = Shared.Transformation;
+using Autodesk.Revit.Attributes;
+using System.Diagnostics;
+
+namespace MEPUtils.PipingSystemsAndFilters
+{
+    [Transaction(TransactionMode.Manual)]
+    class CreatePSLegend : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+
+            using (Transaction tr = new Transaction(doc, "Create PS Legend"))
+            {
+                tr.Start();
+                try
+                {
+                    View view = doc.ActiveView;
+
+                    var psts =
+                        new FilteredElementCollector(doc)
+                        .OfClass(typeof(PipingSystemType));
+                        //.ToDictionary(x => x.Name);
+
+                    foreach (PipingSystemType pst in psts)
+                    {
+                        Color color = pst.LineColor;
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    tr.RollBack();
+                    throw;
+                }
+                tr.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
